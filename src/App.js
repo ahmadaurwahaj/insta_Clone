6ae4e56c1377ede6ded00a68dd06d015fc1e0afd
Wahaj Choudhry <wahaj_choudhry@wahajs-mbp.home>
2020-09-03 20:31:50 +0500

    message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [media, setMedia] = useState({});
+  const access_token =
+    "IGQVJYRlpWemRfclRZAbjV1UmZAXZAnZA4YnlJblJBVXhRMjhkenV6MTNsSXkxUE01dllpeWI3cmJOSWxOMHRZAZAk1raGcyUHdZAeG94dS11WmlVYWg3M3F2UkNlZA1hQMGluUXY1c3hSOUFINTlXTExZAOUE4aQZDZD";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.instagram.com/v1/self/media/recent?access_token=${access_token}`
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log("Error loading data");
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
