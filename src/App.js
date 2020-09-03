c27ea3b827037dbb58dc8152511e6394ec78ac2d
Wahaj Choudhry <wahaj_choudhry@wahajs-mbp.home>
2020-09-03 21:11:13 +0500

    m

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 export default function App() {
   const [data, setData] = useState({});
-
+  const [error, setError] = useState("");
   const access_Token =
     "IGQVJVVU1mS2pTc3g3eWpCTk8wLXpRVXpLSVpWdG1kUVByX2dlX0lONzRoTHMxY24zRGNMOTJoTW5pNms3NGdSMDJCdnREaUtRZA3FnRGZAENmNiSkktMkRjeGJDdmZAmMVlmWVpTMTFUUkxtU0ZAOUlFKQgZDZD";
   useEffect(() => {
@@ -15,13 +15,14 @@ export default function App() {
         setData(res.data);
       })
       .catch(error => {
-        console.log("Error loading data");
+        setError("Error getting data");
       });
   }, []);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Name - {data.full_name}</h2>
+      <h2>Error - {error}</h2>
     </div>
   );
 }
