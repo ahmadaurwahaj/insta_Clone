eb7e003135e8f74072e57b0780e1a9ec60179073
chwahajahmad <chwahajahmad@gmail.com>
2020-09-25 15:33:08 +0500

    for github

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -4,14 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXPqTr_7QvxXZkBAMQgtagJCb7eGP0cz0",
-  authDomain: "instaclone-3323c.firebaseapp.com",
-  databaseURL: "https://instaclone-3323c.firebaseio.com",
-  projectId: "instaclone-3323c",
-  storageBucket: "instaclone-3323c.appspot.com",
-  messagingSenderId: "971871611877",
-  appId: "1:971871611877:web:22458129939be0fd7f9d0c",
-  measurementId: "G-CD211VDRP8",
+  //paste your firebase config here
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
